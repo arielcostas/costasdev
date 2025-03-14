54dab1e00b38693e96c801d0c5a020693a35bbda
Ariel Costas Guerrero <94913521+arielcostas@users.noreply.github.com>
2025-03-14 22:21:47 +0100

    Refactor SCSS imports to use @use syntax, update portfolio entries, and add new projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
-	compressHTML: true,
-	site: "https://www.costas.dev",
-	integrations: [sitemap({
-		priority: 0.5,
-		changefreq: 'weekly'
-	})],
-	build: {
-		assets: 'assets',
-		inlineStylesheets: 'never',
-	},
+    compressHTML: true,
+    site: "https://www.costas.dev",
+    integrations: [sitemap({
+        priority: 0.5,
+        changefreq: 'weekly'
+    }), mdx()],
+    build: {
+        assets: 'assets',
+        inlineStylesheets: 'never',
+    },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,24 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	loader: glob({ pattern: '**/*.md', base: "src/data/blog" }),
+	loader: glob({ pattern: '**/*.{md,mdx}', base: "src/data/blog" }),
 	schema: z.object({
 		title: z.string(),
 		metaDescription: z.string(),
-		publishedAt: z.date()
+		publishedAt: z.coerce.date()
 	})
 });
 
+const portfolio = defineCollection({
+	loader: glob({ pattern: '**/*.{md,mdx}', base: "src/data/portfolio" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		technologies: z.array(z.string())
+	})
+})
+
 export const collections = {
-	blog
+	blog,
+	portfolio
 };
