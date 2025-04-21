b4ef1a0d288565f744bf754af456c4f60da99ca7
Ariel Costas Guerrero <ariel@costas.dev>
2025-04-21 22:54:15 +0200

    Refactor localization: remove i18n support and update layouts to use static text

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,12 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
-import { DEFAULT_LANGUAGE, LANGUAGE_CODES } from "./src/i18n";
 
 import icon from "astro-icon";
 
 export default defineConfig({
   compressHTML: true,
   site: "https://www.costas.dev",
-  i18n: {
-    defaultLocale: DEFAULT_LANGUAGE,
-    locales: LANGUAGE_CODES,
-    routing: {
-      prefixDefaultLocale: false,
-      fallbackType: "redirect",
-      redirectToDefaultLocale: true,
-    },
-  },
   integrations: [
     sitemap({
       priority: 0.5,
