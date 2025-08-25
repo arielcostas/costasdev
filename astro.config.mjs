4cac4f32a42c8f5a8490ed5018c30a279b1f5eb0
Ariel Costas Guerrero <ariel@costas.dev>
2025-08-25 17:56:18 +0200

    Add compression plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,15 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import rehypeExternalLinks from "rehype-external-links";
 
+import compressor from "astro-compressor";
+
 export default defineConfig({
   compressHTML: true,
   site: "https://www.costas.dev",
-  integrations: [
-    sitemap({
-      priority: 0.5,
-      changefreq: "weekly",
-    }),
-    mdx(),
-    icon(),
-  ],
+  integrations: [sitemap({
+    priority: 0.5,
+    changefreq: "weekly",
+  }), mdx(), icon(), compressor()],
   build: {
     assets: "assets",
     inlineStylesheets: "never",
